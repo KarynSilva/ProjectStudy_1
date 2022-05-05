14eed9fee9ef439e4553fe6cf38491d8f2eaf0fd
Karyn Silva <k.silva@4intelligence.com.br>
2022-05-05 15:14:48 -0300

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+import json
+from fastapi import FastAPI,requests
 from pydantic import BaseModel
 
 
 class Food(BaseModel):
+    id:str
     name: str
     origination:str 
     created: str
@@ -10,52 +12,48 @@ class Food(BaseModel):
 
 app = FastAPI()
 
-food_db = [{"name": "Panna Cotta", "origination":"Piemonte - Italy", "created":"20th century"},
-{"name": "Croissant", "origination":"Viena - Austria", "created":"1683"},
-{"name": "Capuccino", "origination":"Brasil", "created":"16th or 17th century"},
-{"name": "Gianduia", "origination":"Piemonte - Italy", "created":"1806 - 19th century"},
-{"name": "Crème brûlée", "origination":"France", "created":"17th century"},
-{"name": "Macarons", "origination":"Veneto - Italy", "created":"16th century"}]
+food_db = [{"id": "0","name": "Panna Cotta", "origination":"Piemonte - Italy", "created":"20th century"},
+{"id": "1","name": "Croissant", "origination":"Viena - Austria", "created":"1683"},
+{"id": "2","name": "Capuccino", "origination":"Brasil", "created":"16th or 17th century"},
+{"id": "3","name": "Gianduia", "origination":"Piemonte - Italy", "created":"1806 - 19th century"},
+{"id": "4","name": "Crème brûlée", "origination":"France", "created":"17th century"},
+{"id": "5","name": "Macarons", "origination":"Veneto - Italy", "created":"16th century"}]
 
 #listar
 @app.get("/foods/")
-async def read_food_list(skip:int = 0, limit:int=10):
-    print("read_food_list\n")
-    return food_db[skip : skip + limit]
-
-#get by id
-@app.get("/foods/{food_id}")
-async def read_food(food_id: str):
-    
-    return food_db[int(food_id)]
-
+async def read_food_list(skip:int = 0, limit:int=10):return food_db[skip : skip + limit]  
 #criar
 @app.post("/foods/")
-async def insert_food(food:Food):
-    print("insert_food\n")
+async def create_food(food:Food):
+    food.id = len(food_db)
     food_db.append(food)
-    return food
-
+    return 'Data entered successfully,please list again to see it!'
+#get by id
+@app.get("/foods/{food_id}")
+async def read_food(food_id: str):return food_db[int(food_id)]
 #editar
 @app.put("/foods/{food_id}")
-async def update_food(food_id: str, food: Food):
-    print("update_food\n")
-    results = {"food_id": food_id, "food": food}
-    return food_db[int(food_id)]
-    return results
+async def update_food(food: Food):
+    food_db[int(food.id)] = food
+    return 'Data changed successfully,please list again to see it!'
+    
 
 @app.delete("/foods/{food_id}")
 async def delete_food(food_id: str):
-    print("delete_food")
-    food_db.remove(food_id)
-    return "ID:"+{"food_id": food_id}+" REMOVIDO!"
+    #food_db[int(food_id)].update({"name": "None", "origination":"None", "created":"None"})
+    return 'Data removed successfully,please list again to see it!'
 
+food_insert = {
+        "id":"None",
+        "name": "Pizza",
+        "origination": "Campania - Italy",
+        "created": "18th century"
+              }
 
-food_insert = {"name": "Pizza", "origination":"Campania - Italy", "created":"18th century"}
-food_edit = {"name": "Capuccino", "origination":"Italy", "created":"16th or 17th century"}
-
-read_food_list(2,3)
-read_food("2")
-#insert_food(food_insert)
-#update_food("2",food_edit)
-#delete_food("5")
+food_edit =   {
+        "id":"1",
+        "name": "Capuccino",
+        "origination": "Italy",
+        "created": "16th or 17th century"
+              }
+#http://127.0.0.1:8000/foods/
