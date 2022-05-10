6eecb9bfb5bcc8323cde2ba118d869ab736ff45b
Karyn Silva <k.silva@4intelligence.com.br>
2022-05-10 09:09:12 -0300

    All Http operantions working

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import datetime,json
+from xxlimited import foo
 from pymongo import MongoClient
 from fastapi import FastAPI,requests
 from pydantic import BaseModel
+import operations as op
 
 app = FastAPI()
 
@@ -13,50 +15,73 @@ class Food(BaseModel):
     created: str
     date: str
 
-    
+   
 client = MongoClient()
 client = MongoClient('localhost', 27017)
+food_db = []
 db = client.FoodsDatabase
-food_db = list(db.foodscoll.find())
+
 
 @app.get("/foods/")
-async def read_food_list(skip:int =0, limit:int=10):return food_db[skip : skip + limit]  
+def read_food_list(skip:int =0, limit:int=10):
+
+    food_db = list(db.foodscoll.find())
+    if(not food_db):
+        op.start() 
+        food_db = list(db.foodscoll.find())
+    return food_db[skip : skip + limit]  
 
 #criar
 @app.post("/foods/")
-async def create_food(food:Food):
-    food.date = str(datetime.datetime.now())
-    db.foodscoll._insert_one(food)
+def create_food(food:Food):
+
+    food_db = list(db.foodscoll.find())
+    food_i = {"_id": str(len(food_db)),"name": food.name,"origination": food.origination,"created": food.created,
+    "date": str(datetime.datetime.now())}
+    db.foodscoll.insert_one(food_i)
     return 'Data entered successfully,please list again to see it!'
 
 #get by id
 @app.get("/foods/{food_id}")
-async def read_food(food_id: str):
+def read_food(food_id: str):
+
+    food_db = list(db.foodscoll.find())
     return food_db[int(food_id)]
 #editar
 @app.put("/foods/{food_id}")
-async def update_food(food: Food):
-    db.foodscoll.replace_one({"id_" : food.id},{"name":food.name,"origination":food.origination,
+def update_food(food: Food):
+
+    food_db = list(db.foodscoll.find())
+    for i in range(len(food_db)):
+        if(food_db[i]["name"]==food.name):
+            food_id = food_db[i].get("_id")
+
+    db.foodscoll.replace_one({"_id" : food_id},{"name":food.name,"origination":food.origination,
     "created":food.created,"date": str(datetime.datetime.now())})
-    food_db = db.foodscoll
+
     return 'Data changed successfully,please list again to see it!'
     
 
 @app.delete("/foods/{food_id}")
-async def delete_food(food_id: str):
-    db.foodscoll.delete_many({"id_":food_id})
-    food_db = db.foodscoll
+def delete_food(food_id: str):
+    food_db = list(db.foodscoll.find())
+    db.foodscoll.delete_many({"_id":food_id})
+    food_db = db.foodscoll.find()
     return 'Data removed successfully,please list again to see it!'
 
-food_insert = {"name": "Pizza",
+
+    
+'''
+food_insert = {"_id":"None","name": "Pizza",
         "origination": "Campania - Italy",
         "created": "18th century",
         "date": "None"
               }
 
-food_edit =   {"name": "Capuccino",
+food_edit =   {"_id":"2","name": "Capuccino",
         "origination": "Italy",
         "created": "16th or 17th century",
         "date": "None"
               }
 
+'''
--- operations.py
+++ operations.py
@@ -10,7 +10,7 @@ db = client.FoodsDatabase
 
 def start():
     date_now = str(datetime.datetime.now())
-
+    
     f_list = [{"_id":'0', "name": "Panna Cotta", "origination":"Piemonte - Italy", "created":"20th century","date": date_now},
     {"_id":'1',"name": "Croissant", "origination":"Viena - Austria", "created":"1683","date": date_now},
     {"_id":'2',"name": "Capuccino", "origination":"Brasil", "created":"16th or 17th century","date": date_now},
