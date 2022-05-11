7ea17884ffc650d0edd184d348a0d2a5e93d580b
Karyn Silva <k.silva@4intelligence.com.br>
2022-05-11 17:04:55 -0300

    creating the initials bases for the application construct  with dockerfile and compose

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import datetime
 from pymongo import MongoClient
 from fastapi import FastAPI
 from pydantic import BaseModel
-import operations as op
+import app.operations as op
 
 app = FastAPI()
 
