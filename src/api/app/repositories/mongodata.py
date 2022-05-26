9fe1f7c2f9e5d76fb507a8049e29fcaed023b68f
Marcelo Ikejima <imitsuo@gmail.com>
2022-05-26 14:12:46 -0300

    comentarios

--- src/api/app/repositories/mongodata.py
+++ src/api/app/repositories/mongodata.py
@@ -6,14 +6,28 @@ import datetime
 client = MongoClient('mongodb://karyn:pass@mongo-project:27017/?authSource=admin&authMechanism=SCRAM-SHA-1', connect=True)
 db = client.Foods
 
+# nome de classes em python acaba seguindo o padrão do Java
+# FoodRepository, tenho preferencia por usar no singular a entidade que está sendo persistida no repositorio
+# Normalmente eu não coloco no nome das classes, funções ou metodos particularidades da implementação
+# exemplo Mongo que é o banco de dados, ou int_codigo_cliente, somente em casos especificos.
+# no futuro se você mudar a implementação de Mongo para Sql Server ou Postgres o FoodRepository se mantem valido.
+# e mantemos a perspectiva do encapsulamento
+
 class Mongo_Foods_Repository(): 
 
     food_db = []
 
     def __init__(self):
       self.food_db = list(db.foodscoll.find())
+      # criaria uma variavel aqui
+      # self.food_collection = db.foodscoll
 
     def list(self):
+
+        # no sentido de o metodo ter somente uma responsabilidade, 
+        # cria um outro arquivo fora do repositorio
+        # que faz o insert inicial dos registros
+
         if(not self.food_db):
              date_now = str(datetime.datetime.now())
     
@@ -29,9 +43,16 @@ class Mongo_Foods_Repository():
              db.foodscoll.insert_many(f_list)
              self.food_db = list(db.foodscoll.find()) 
 
+        # itens = self.food_collection.find()
+        # converte o dictionary em objeto do schema Food
+        # 
+
         return self.food_db
 
     def create(self,food:Food):
+
+        # é possivel usar o 
+        #food.dict() para criar o dicionario food.dict()
 
         food_i = {
                   "_id": str(len(self.food_db)), "name": food.name, "origination": food.origination, "created": food.created,
@@ -41,6 +62,7 @@ class Mongo_Foods_Repository():
         self.food_db = list(db.foodscoll.find())
 
     def list_by_id(self, food_id : str):
+        #
         return self.food_db[int(food_id)]
 
     def edit(self,food:Food):
